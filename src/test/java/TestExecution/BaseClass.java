373ec1af3967cd8647a98a5ac0ed2c537a47708b
mahendran <mahendran@xmedia.in>
2024-01-02 16:27:11 +0530

    documentation commit 1

--- src/test/java/TestExecution/BaseClass.java
+++ src/test/java/TestExecution/BaseClass.java
@@ -9,12 +9,15 @@ public class BaseClass {
 
 	@BeforeClass
 	public void setup() {
+	//Configure the browser tha you want to test, keep your browser under the webdriver folder
 	//chrome || edge  || firefox
 	Configuration.openBrowser("edge");
 	Configuration.openApplication("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 	
 	
 	}
+	
+	//This action will close your current browser
 	
 	@AfterClass
 	public void teardown() {
